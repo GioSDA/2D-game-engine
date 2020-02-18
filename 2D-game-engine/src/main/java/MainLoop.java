604ba96327c1ce502e2a785631d41cad27a7c5cf
GioSDA <gsdambrosio@gmail.com>
2020-02-17 21:17:40 -0800

    Changed formatting and fixed some code

---
+++ 2D-game-engine/src/main/java/MainLoop.java
@@ -0,0 +1,31 @@
+package main.java;
+
+import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+import main.java.render.Screen;
+
+public class MainLoop{
+	public static JFrame frame = new JFrame();
+	
+	public File file = new File(MainLoop.class.getClassLoader().getResource("GameIcon.png").getFile());
+	
+	public static void main(String[] args) {
+		try {
+			MainLoop mainloop = new MainLoop();
+			Screen screen = new Screen(frame, new Dimension(400, 400), ImageIO.read(mainloop.file));
+			screen.enableFrame(frame);
+			while (true) {
+				screen.render(ImageIO.read(mainloop.file));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+}
