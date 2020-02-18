9be37599446b803b401dbe7a553feff7bcd5211f
GioSDA <gsdambrosio@gmail.com>
2020-02-17 18:09:15 -0800

    Added Image to be rendered

--- 2D-game-engine/src/main/MainLoop.java
+++ 2D-game-engine/src/main/MainLoop.java
@@ -1,18 +1,23 @@
 package main;
 
 import java.awt.Dimension;
+import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 import render.Screen;
 
-public class MainLoop {
+public class MainLoop{
 	public static JFrame frame = new JFrame();
 	public static void main(String[] args) {
 		try {
 			Screen screen = new Screen(frame, new Dimension(400, 400));
 			screen.enableFrame(frame);
+			while (true) {
+				screen.render(ImageIO.read(new File("C:/Users/Giovanni.ClarescoInc-PC/git/2D-game-engine/2D-game-engine/res/images/GameIcon.png")));
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(0);
